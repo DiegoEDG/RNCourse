db3f036188d5ce37e1fafb6c56f81d1f6b382f49
Digudev <1.5.dg.diego@gmail.com>
2023-06-20 07:55:44 -0600

    INIT

---
+++ src/screens/MainScreen.tsx
@@ -0,0 +1,13 @@
+import {SafeAreaView, Text, View} from 'react-native';
+
+const MainScreen = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>RN Course</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default MainScreen;
